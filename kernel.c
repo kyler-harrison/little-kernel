dba466c13a6f85a09e35a938a26e89cf31c19d99
Kyler <harri828@purdue.edu>
2021-07-22 00:25:06 -0400

    handles line wrapping in terminal screen

--- kernel.c
+++ kernel.c
@@ -7,8 +7,9 @@
 
 unsigned short *terminal_buff;
 unsigned int vga_idx;
-unsigned int new_line = 80;  // how much to add to vga_idx to get to next line
+unsigned int newline = 80;  // how many characters fit in one line of screen 
 
+// empty screen
 void clear_screen(void) {
 	int idx = 0;
 	// 25 lines of 80 columns (ascii chars), each elem is 2 bytes
@@ -19,22 +20,32 @@ void clear_screen(void) {
 	}
 }
 
+// move to next line on screen
+void vga_newline(void) {
+	vga_idx = (vga_idx / newline + 1) * newline;  // get to start idx of current line, get to start idx of next
+}
+
 // char in output defined by 2 bytes: str byte and color byte
 void print_str(char *str, unsigned char color) {
 	int idx = 0;
 
 	while (str[idx]) {
-		// this makes a 2 byte (16 bit input) where the first 8 bits represent the color
+		// this makes a 2 byte (16 bit) input where the first 8 bits represent the color
 		// and the second 8 bits represent the character
 		if (str[idx] != '\n') {
 			terminal_buff[vga_idx] = (unsigned short) str[idx] | (unsigned short) color << 8;
 			vga_idx++;
-			// TODO add check for line wrapping
+
 		} else {
-			vga_idx = (vga_idx / new_line + 1) * new_line;  // get to start idx of current line, get to start idx of next
+			vga_newline();
 		}
 
 		idx++;
+	}
+
+	// a single str automatically wraps, but a newline may still be needed at the end
+	if (idx % newline > 0 && idx != 0) {
+		vga_newline();
 	}
 } 
 
@@ -43,7 +54,7 @@ void main(void) {
 	vga_idx = 0;
 	clear_screen();
 
-	print_str("welcome \nto screen\n", GREEN);
+	print_str("welcome \nto screen\nmore chars and chars and chars and is that eighty yet i dont know i think\nthis will just wrap anyways", GREEN);
 	print_str("fare\nwell", RED);
 
 	return;
