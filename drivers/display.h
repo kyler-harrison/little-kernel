2d60cc7e8b991efe53e3c176dcf867d3ecaa3a0e
Kyler <harri828@purdue.edu>
2021-07-25 02:30:21 -0400

    cursor addresses

--- drivers/display.h
+++ drivers/display.h
@@ -1,4 +1,8 @@
 #define VGA_ADDR 0xB8000  
+#define VGA_LOW 0x0F
+#define VGA_HIGH 0x0E
+#define CURSOR_CTRL_ADDR 0x3D4
+#define CURSOR_DATA_ADDR 0x3D5
 #define ROWS 25
 #define COLS 80
 #define MAX_IDX ROWS * COLS
