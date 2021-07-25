8f113f1d6d80487de5d2aaabd9c54a3bd9f67230
Kyler <harri828@purdue.edu>
2021-07-25 01:33:30 -0400

    functions for reading/writing to specific device addresses

---
+++ drivers/ports.c
@@ -0,0 +1,8 @@
+void write_port(unsigned short port, unsigned char data) {
+	asm("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+unsigned char read_port(unsigned short port) {
+	unsigned char data;
+	asm("in %%dx, %%al" : "=a" (data) : "d" (port));  
+}
---
+++ drivers/ports.h
@@ -0,0 +1,2 @@
+void write_port(unsigned short port, unsigned char data);
+unsigned char read_port(unsigned short port);
