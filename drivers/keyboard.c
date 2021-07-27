4edb40fe615461c834f7302883c7f08b85e1ea61
Kyler <harri828@purdue.edu>
2021-07-27 03:03:45 -0400

    can handle keypress inputs

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -2,14 +2,20 @@
 #include "display.h"
 #include "ports.h"
 
+#define num_keys 88  // TODO move to .h
+
+// hex keyboard scancodes: https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
+// space either means space or tab 
+// special key indexes: 0:err, 1:esc, 12:backspace, 26:enter, 27: Lctrl, 42:Lshift, 54:Rshift, 55:prntscrn, 56:LALT
+char *keys[num_keys] = {"ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BACKSPACE", " ", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "ENTER", "LCTRL", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "LSHIFT", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "RSHIFT", "PRNTSCRN", "LALT", " "};
+
 // print input back if valid 
 void keypress(unsigned char input) {
-	switch (input) {
-		case 0x1C :
-			print_str("VALID ENTER\n", RED);
-			break;
-		default:
-			break;
+	int idx = ((int) input) - 1;
+
+	if (idx < num_keys) {
+		// TODO catch idx and handle LSHIFT/RSHIFT, ENTER, BACKSPACE
+		print_str(keys[idx], WHITE);
 	}
 }
 
@@ -20,10 +26,11 @@ void repl(void) {
 		// read value (byte) at keyboard addr into input variable
 		input = read_port(KEYBOARD_ADDR);
 
-		if (input == 0x1C) {
+		// haven't figured out the actual default scancode yet, so this max is kinda arbitrary
+		if (input != 0x0 && input <= 0x80) {
 			keypress(input);
 			input = 0x0;
-			write_port(KEYBOARD_ADDR, input);
+			write_port(KEYBOARD_ADDR, input);  // set back to err code
 		}
 	}
 }
