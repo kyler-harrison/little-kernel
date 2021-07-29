85f432762b4c894176399c59e4b0884e5ef3ad5f
Kyler <harri828@purdue.edu>
2021-07-29 01:37:25 -0400

    hacky, but backspace and arrows work

--- drivers/display.c
+++ drivers/display.c
@@ -1,16 +1,28 @@
 #include "display.h"
 #include "ports.h"
+#include "utils.h"
 
 unsigned short *terminal_buff = (unsigned short *) VGA_ADDR;  // vga terminal display
 unsigned int vga_idx = 0;  // track end of line
-int cursor_idx = 0;  // track cursor in terminal
-int lower_bound = 1;  // refers to idx of a single line, i.e. the ">" in terminal (will need to update once dirs implemented) 
+int lower_bound = 1;  // refers to user start idx of a single line, i.e. the ">" in terminal (will need to update once dirs implemented) 
 int current_line = 0;
+char input_buffer[MAX_BUFFER_SIZE];
+int buffer_idx = 0;
+
+// add char to input_buffer array (string)
+void append_buffer(char val) {
+	input_buffer[buffer_idx] = val;
+	buffer_idx++;
+}
+
+// kind of a lie, but input_buffer will only be read up to buffer_idx
+void clear_buffer(void) {
+	buffer_idx = 0;
+}
 
 // empty screen
 void clear_screen(void) {
 	// screen is 25 lines of 80 elems, elems are unsigned shorts (2 bytes to represent character and its color)
-	
 	int i;
 	for (i = 0; i < COLS * ROWS; i++) {
 		terminal_buff[i] = (unsigned short) WHITE << 8 | (unsigned short) ' ';
@@ -27,20 +39,19 @@ void set_cursor(int pos) {
 }
 
 // update the index tracking the current cursor position
-void update_cursor_idx(int offset) {
-	int check = cursor_idx + offset;
+void update_vga_idx(int offset) {
+	int check = vga_idx + offset;
 
 	// cursor is bounded to line
-	if ((offset == -1 && check > (current_line * COLS + 1 + offset)) || (offset == 1 && check <= vga_idx)) {
-		cursor_idx += offset;
-		set_cursor(cursor_idx);
+	if ((offset == -1 && check > (current_line * COLS + 1 + offset)) || offset == 1) {
+		vga_idx += offset;
+		set_cursor(vga_idx);
 	}
 }
 
 // move to next line on screen
 void vga_newline(void) {
 	vga_idx = (vga_idx / COLS + 1) * COLS;  // get to start idx of current line then get to start idx of next
-	cursor_idx = vga_idx;
 	current_line = vga_idx / COLS;
 }
 
@@ -59,14 +70,27 @@ void shift(void) {
 	}
 
 	vga_idx = MAX_IDX - COLS;
-	cursor_idx = cursor_idx - COLS;  // TODO check if this is right
 	current_line = vga_idx / COLS;
 }
 
 // char in output defined by 2 bytes: str byte and color byte
 void print_str(char *str, unsigned char color) {
-	int idx = 0;
+	int update = 1;
 
+	// note that you can't actually edit if you move cursor
+	if (str_comp(str, "BACKSPACE")) {
+		update_vga_idx(-1);
+		str = " ";
+		update = 0;
+	} else if (str_comp(str, "LARROW")) {
+		update_vga_idx(-1);
+		return;
+	} else if (str_comp(str, "RARROW")) {
+		update_vga_idx(1);
+		return;
+	}
+
+	int idx = 0;
 	while (str[idx]) {
 		// 2 byte input where first byte represents color and second represents character
 		if (str[idx] != '\n') {
@@ -77,13 +101,14 @@ void print_str(char *str, unsigned char color) {
 			}
 
 			terminal_buff[vga_idx] = (unsigned short) color << 8 | (unsigned short) str[idx];
-			vga_idx++;
-			cursor_idx++;
-			set_cursor(cursor_idx);
+			if (update) {
+				vga_idx++;
+				set_cursor(vga_idx);
+			}
 
 		} else {
 			vga_newline();
-			set_cursor(cursor_idx);
+			set_cursor(vga_idx);
 		}
 
 		idx++;
@@ -92,7 +117,7 @@ void print_str(char *str, unsigned char color) {
 	// a single str automatically wraps, but a newline may still be needed at the end
 	if (idx / COLS > 0 && idx != 0 && str[idx - 1] != '\n') {
 		vga_newline();
-		set_cursor(cursor_idx);
+		set_cursor(vga_idx);
 	}
 } 
 
