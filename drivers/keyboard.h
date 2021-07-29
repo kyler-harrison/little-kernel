469cf71900475dca39e0595b1b87a2525a1f09d5
Kyler <harri828@purdue.edu>
2021-07-29 00:53:19 -0400

    headers moved to display.h

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,8 +1,5 @@
 #define KEYBOARD_ADDR 0x60  
 #define NUM_KEYS 77
-#define MAX_BUFFER_SIZE 1000
 
-void append_buffer(char val);
-void clear_buffer(void);
-int keypress(unsigned char input);
+void keypress(unsigned char input);
 void repl(void);
