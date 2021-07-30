d3134393d6c3516876391150f14403f53de3e0f7
Kyler <harri828@purdue.edu>
2021-07-29 20:33:32 -0400

    simple working input buffer implemented

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -11,30 +11,60 @@
 char *keys[NUM_KEYS] = {"", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BACKSPACE", " ", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "ENTER", "", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "`", "LSHIFT", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "RSHIFT", "", "", " ", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "LARROW", "", "RARROW"};
 //char *shift_keys[NUM_KEYS] = {};  // TODO
 int shift_on = 0;
+char input_buffer[MAX_BUFFER_SIZE];
+int buffer_end = 0;  // current idx of buffer to alter
+
+// add char to input_buffer array (string)
+void update_buffer(char val) {
+	input_buffer[buffer_end] = val;
+	buffer_end++;
+}
+
+// kind of a lie, but input_buffer will only be read up to idx of buffer_size - 1
+void clear_buffer(void) {
+	buffer_end = 0;
+}
 
 // process keyboard input, returns 1 if newline needed (i.e. print out terminal pwd prompt again)
 void keypress(unsigned char input) {
 	int idx = ((int) input) - 1;  // scancodes are conveniently in order (except for 0th scancode)
-	char *buf;
 
 	if (idx < NUM_KEYS) {
 		char *key = keys[idx];
 
 		if (str_comp(key, "LSHIFT") || str_comp(key, "RSHIFT")) {
 			shift_on = shift_on ? 0 : 1;  // shift will work like caps lock key
-			return;
-		}
-
-		if (shift_on) {
-			//key = shift_keys[idx];  // TODO
-		}
-
-		if (str_comp(key, "ENTER")) {
+		} else if (str_comp(key, "LARROW") || str_comp(key, "RARROW")) {
+			// not handling these for the moment
+		} else if (str_comp(key, "ENTER")) {
+			// fun from somewhere to process buffer (input is input_buffer[0] to input_buffer[buffer_end - 1])
 			clear_buffer();
 			print_str("\n", WHITE);
+			print_str("buffer: ", WHITE);
+
+			// get the actual inputted values (input_buffer may contain more than user would expect)
+			char *input;
+			for (int i = 0; i < buffer_end; i++) {
+				input[i] = input_buffer[i];
+			}
+
+			print_str(input_buffer, WHITE);
+			print_str("\n", WHITE);
 			print_str(TERM_PROMPT, TERM_COLOR);
+
+		} else if (str_comp(key, "")) {
+			// other unhandled special keys
+		} else if (str_comp(key, "BACKSPACE")) {
+			// TODO
+			// decrement buffer idx
+			// call update_vga_idx(-1)
 		} else {
-			print_str(key, WHITE);  // arrows, enter, "", and backspace all handled in display
+			if (shift_on) {
+				//key = shift_keys[idx];  // TODO
+			}
+
+			update_buffer(key[0]);  // safe to do bc key should only be a single char here
+			print_str(key, WHITE);
 		}
 	} 
 }
@@ -52,6 +82,9 @@ void repl(void) {
 			keypress(input);
 			input = 0x0;
 			write_port(KEYBOARD_ADDR, input);  // set back to err code
+			//print_str("buffer:\n", WHITE);
+			//print_str(input_buffer, WHITE);
+			//print_str("\n", WHITE);
 
 			/*
 			if (nl_eval) {
--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,5 +1,8 @@
 #define KEYBOARD_ADDR 0x60  
 #define NUM_KEYS 77
+#define MAX_BUFFER_SIZE 100
 
+void update_buffer(char val);
+void clear_buffer(void);
 void keypress(unsigned char input);
 void repl(void);
